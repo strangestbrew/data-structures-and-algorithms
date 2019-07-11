2add8b847090244b20fc08e9976b1d4f0721179d
strangestbrew <thomas.lyndsey@gmail.com>
2019-07-11 14:20:14 -0700

    started

--- code-challenges/sort/challenges-03.test.js
+++ code-challenges/sort/challenges-03.test.js
@@ -7,7 +7,9 @@ Write a function named sortBackwards that takes in an array of numbers and retur
 ------------------------------------------------------------------------------------------------ */
 
 const sortBackwards = (arr) => {
-  // Solution code here...
+  arr.sort(a,b) => {
+    return a < b;
+  }
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -21,7 +23,9 @@ For example, ['Alphabet', 'Zebra', 'alphabet', 'carrot'] is correctly sorted.
 ------------------------------------------------------------------------------------------------ */
 
 const alphabetize = (arr) => {
-  // Solution code here...
+  arr.sort() => {
+
+  }
 };
 
 /* ------------------------------------------------------------------------------------------------
