f72cbae662724271c352a5c99e9294e1cd2ab3ba
strangestbrew <thomas.lyndsey@gmail.com>
2019-07-13 13:12:58 -0700

    challenge1 question four

--- code-challenges/for-each/challenges-01.test.js
+++ code-challenges/for-each/challenges-01.test.js
@@ -41,8 +41,8 @@ const addValues = (arr, value) => {
 };
 
 const addNumbers = (num, arr, times, callback) => {
-  for (let  = i; i < time; i++) {
-    addValue(arr, num);
+  for (let i = 0; i < times; i++) {
+    callback(arr, num);
   }
   return arr;
 };
@@ -60,11 +60,17 @@ Return the modified array.
 ------------------------------------------------------------------------------------------------ */
 
 const removeOne = (num, arr) => {
-  // Solution code here...
+  if( num % 3 === 2 ) {
+    arr.pop();
+  }
+  return arr;
 };
 
 const removeElements = (arr, callback) => {
-  // Solution code here...
+  for (let i = 0; i < arr.length; i++) {
+    callback(arr[i], arr);
+  }
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
