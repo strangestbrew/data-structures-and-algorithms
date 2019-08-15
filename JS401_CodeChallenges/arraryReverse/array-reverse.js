a88f8cbbc9e1a5f62a37d33e01a37b961c677f98
LYNDSEY THOMAS <THOMAS.LYNDSEY@GMAIL.COM>
2019-08-15 15:44:36 -0700

    arrayReverse

--- JS401_CodeChallenges/arraryReverse/array-reverse.js
+++ JS401_CodeChallenges/arraryReverse/array-reverse.js
@@ -1,1 +1,19 @@
+'use strict';
+
 //Write a function called reverseArray which takes an array as an argument. Without utilizing any of the built-in methods available to your language, return an array with elements in reversed order.
+
+
+let arr = [1, 3, 5, 7, 2000, -16];
+let arr2 = ['never', 'gonna', 'give', 'you', 'up'];
+console.log(arr, 'hello moto');
+
+function reverseArrayInPlace(arr) {
+  for (var i = 0; i <= (arr.length - 1) / 2; i++) {
+      let indexValue = arr[i];
+      arr[i] = arr[arr.length - 1 - i];
+      arr[arr.length - 1 - i] = indexValue;
+  }
+  console.log('line 12');
+  return arr;
+}
+reverseArrayInPlace(arr2);
