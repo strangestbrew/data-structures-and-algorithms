cbd4267bacf872474ada1273765eee22ad9f27a8
LYNDSEY THOMAS <THOMAS.LYNDSEY@GMAIL.COM>
2019-08-25 21:13:19 -0700

    enqueue dequeue

--- JS401_CodeChallenges/stacksAndQueues/stacks-and-queues.js
+++ JS401_CodeChallenges/stacksAndQueues/stacks-and-queues.js
@@ -1,2 +1,47 @@
 'use strict';
 
+import { declareOpaqueType } from "@babel/types";
+
+let queue = [];
+
+queue.unshift(1);
+
+queue.unshift(2);
+
+queue.unshift(3);
+
+queue.pop();
+
+queue.push();
+
+class Node {
+    //pointer to
+    //pointer from 
+}
+
+class Stack {
+    constructor() {
+        storage = []; //linkedlist instead of array
+        this.storage = new LinkedList(); 
+    }
+
+    push(value){
+        this.storage.push(value);
+        this.append(value);
+
+    }
+    pop(){
+        return this.removeAtEnd(); //code this
+        // return this.storage.pop();
+    };
+
+    //isEmpty();
+    //peek();
+    //lenght(); 
+}
+
+class Queue {
+    enqueue()
+    dequeue()
+    peek()
+}
