205200e482e40d793a442805b772390bc0d2f843
LYNDSEY THOMAS <THOMAS.LYNDSEY@GMAIL.COM>
2019-09-04 20:56:06 -0700

    node creation

--- JS401_CodeChallenges/countLeaves/countLeaves.js
+++ JS401_CodeChallenges/countLeaves/countLeaves.js
@@ -1,2 +1,40 @@
 'use strict';
 
+class Node {
+  constructor(value) 
+  { 
+      this.value = value; 
+      this.left = null; 
+      this.right = null; 
+  } 
+} 
+
+
+class Tree { 
+  constructor() 
+    {
+      this.root = null; 
+    } 
+  
+  insert(value){ 
+    let newNode = new Node(value); 
+	  if(this.root === null) 
+		  this.root = newNode; 
+	  else
+		  this.insertNode(this.root, newNode); 
+  } 
+
+  insertNode(node, newNode){
+    if(newNode.value < node.value){
+		  if(node.left === null) 
+			  node.left = newNode; 
+		else
+			this.insertNode(node.left, newNode); 
+	} else { 
+    if(node.right === null) 
+			node.right = newNode; 
+		else
+			this.insertNode(node.right,newNode); 
+	  } 
+  }
+};
